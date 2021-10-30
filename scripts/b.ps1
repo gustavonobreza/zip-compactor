d4648b4aaa32abf80fc185fd238098635146630f
Gustavo Alexandre Nobre Mesquita <gustavoalexandredev@gmail.com>
2021-10-30 14:40:05 -0300

    chore: add normal build file

---
+++ scripts/b.ps1
@@ -0,0 +1,12 @@
+$filebin = "app.exe";
+
+if (!(Test-Path -Path "go.mod")) {
+   echo "You is a wrong path!!! Go to home dir."
+   exit 1
+}
+
+$originalOS = go env GOHOSTOS
+$originalARCH = go env GOHOSTARCH
+$env:GOOS = $originalOS; $env:GOARCH = $originalARCH;
+
+go install .;
