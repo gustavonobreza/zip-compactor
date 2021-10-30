58bbd838b03cd22b3fdddc49f6b87ab7e5d218f0
Gustavo Alexandre Nobre Mesquita <gustavoalexandredev@gmail.com>
2021-10-29 23:43:40 -0300

    feat(build): build script for all platforms

---
+++ scripts/build-all.ps1
@@ -0,0 +1,44 @@
+$filebin = "zip-compactor";
+$OSs = @('windows', 'linux', 'darwin')
+$basePath = ".\bin\"
+$plataforms = @('amd64','arm64')
+$originalOS = go env GOOS
+$originalARCH = go env GOARCH
+
+Write-Output "Building your app..."
+
+if (!(Test-Path -Path "go.mod")) {
+   echo "You is a wrong path!!! Go to home dir."
+   exit 1
+}
+
+if (Test-Path -Path $basePath) {
+   Remove-Item -Force -Recurse -Confirm:$false $basePath
+}
+
+foreach ($os in $OSs) {
+    foreach ($plataform in $plataforms) {
+        $to = $basePath + $os + "-" + $plataform + ".zip"
+		$binpath = $filebin
+
+		if ($os -eq "windows") {
+			$binpath += '.exe'
+		}
+
+		Write-Output "- Building $os/$plataform"
+
+        # Build
+		$env:GOOS = $os; $env:GOARCH = $plataform; go build -o $binpath .;
+        # Zip to bin folder
+        .\app.exe -from $binpath -to $to;
+        # Delete file
+        Remove-Item -Force -Recurse -Confirm:$false $binpath;
+	}
+}
+
+# Restore initial values
+$env:GOOS = $originalOS;
+$env:GOARCH = $originalARCH;
+
+
+Write-Output "Finished!";
