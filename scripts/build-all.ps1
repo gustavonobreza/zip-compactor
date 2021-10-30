4b952ffcb7b2f50c7c7747ea808e98ead0ed3420
Gustavo Alexandre Nobre Mesquita <gustavoalexandredev@gmail.com>
2021-10-30 15:13:52 -0300

    build: add version flag feat in all binaries

--- scripts/build-all.ps1
+++ scripts/build-all.ps1
@@ -1,41 +1,44 @@
 $filebin = "zip-compactor";
-$OSs = @('windows', 'linux', 'darwin')
-$basePath = ".\bin\"
-$plataforms = @('amd64','arm64')
-$originalOS = go env GOHOSTOS
-$originalARCH = go env GOHOSTARCH
+$OSs = @('windows', 'linux', 'darwin');
+$basePath = ".\bin\";
+$plataforms = @('amd64','arm64');
+$originalOS = go env GOHOSTOS;
+$originalARCH = go env GOHOSTARCH;
 
 $env:GOOS = $originalOS; $env:GOARCH = $originalARCH;
+$env:ZIP_COMPRESSOR_VERSION = git describe --abbrev=0 --tags;
 
-Write-Output "Building your app..."
-Write-Output "ATTENTION: if you stop before you finish you will have problems."
-Write-Output "      SO, if that happens you need to restart the shell"
+
+Write-Output "Building your app...";
+Write-Output "ATTENTION: if you stop before you finish you will have problems.";
+Write-Output "      SO, if that happens you need to restart the shell";
 
 if (!(Test-Path -Path "go.mod")) {
-   echo "You is a wrong path!!! Go to home dir."
-   exit 1
+   echo "You is a wrong path!!! Go to home dir.";
+   exit 1;
 }
 
 if (Test-Path -Path $basePath) {
-  Write-Output "Removing bin folder..."
-  Remove-Item -Force -Recurse -Confirm:$false $basePath | Out-Null
+  Write-Output "Removing bin folder...";
+  Remove-Item -Force -Recurse -Confirm:$false $basePath | Out-Null;
 }
 
-New-Item -Path $basePath -ItemType Directory | Out-Null
+New-Item -Path $basePath -ItemType Directory | Out-Null;
 
 foreach ($os in $OSs) {
     foreach ($plataform in $plataforms) {
-        $to = $basePath + $os + "-" + $plataform + ".zip"
-		$binpath = $filebin
+        $to = $basePath + $os + "-" + $plataform + ".zip";
+		$binpath = $filebin;
 
 		if ($os -eq "windows") {
-			$binpath += '.exe'
+			$binpath += '.exe';
 		}
 
-		Write-Output "- Building $os/$plataform"
+		Write-Output "- Building $os/$plataform";
 
         # Build
-		$env:GOOS = $os; $env:GOARCH = $plataform; go build -o $binpath .;
+		$env:GOOS = $os; $env:GOARCH = $plataform; 
+        go build -ldflags "-X main.Version=$env:ZIP_COMPRESSOR_VERSION" -o $binpath .;
         # Zip to bin folder
         zip-compactor -from $binpath -to $to -q;
         # Delete file
