41c41417ba8687b7cbeed116eab12a19d9500379
Gustavo Alexandre Nobre Mesquita <gustavoalexandredev@gmail.com>
2021-10-30 11:53:50 -0300

    feat: add icon in windows executable

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,8 +1,13 @@
 $filebin = "app.exe";
 
+
 if (!(Test-Path -Path "go.mod")) {
    echo "You is a wrong path!!! Go to home dir."
    exit 1
 }
+
+go install 'github.com/akavel/rsrc'
+
+rsrc -icon .\zip.ico
 
 go build -o $filebin .
